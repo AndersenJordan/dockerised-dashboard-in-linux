3453fbda0ffae0e9d79c6ab011137eb714977285
Jordan Andersen <andersen.jordan@gmail.com>
2023-12-03 21:09:43 +1100

    Update run-me.sh for Linux compatibility 	The order of arguments in the 'docker run...' command was 	initially designed for a Windows environment. The format in this 	commit will now run in a Linux environment.

--- run-me.sh
+++ run-me.sh
@@ -4,10 +4,15 @@ docker pull postgres:11.22-bullseye
 # Create and run a Docker container named 'postgresContainer' using the Docker
 # image for PostgreSQL. The container is assigned to port 5432 with a basic
 # password (pass123).
-docker run -d --name postgresContainer -p 5433:5432 -e POSTGRES_PASSWORD=pass123 postgres:11.22-bullseye
+docker run --name postgresContainer -d -p 5433:5432 -e POSTGRES_PASSWORD=pass123 postgres:11.22-bullseye
 # pip install requirements
 python -m pip install -r requirements.txt
-# Run Python script to generate results for Tests and Viusalisations
-python assessment-results.py
+# Run Python script to generate results for Tests and Viusalisations and notify
+# user whether the script ran successfully
+if python assessment-results.py; then
+    echo "The Python script assessment-results.py was executed succesfully"
+else
+    echo "Python script assessment-results.py failed during execution"
+fi
 # Stop Docker container
 docker stop postgresContainer
